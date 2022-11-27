8edf24abf5ba7c74f100519f8b18201e0681b714
Jonas Tenora <99197211+wiserat@users.noreply.github.com>
2022-11-27 10:06:32 +0100

    Add files via upload

---
+++ Flask-Web-App-Tutorial/website/auth.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        sensor_id = request.form.get('sensor_id_html')
+        list_of_sensor_ids = []
+        
+        with open('website/sensor_ids.txt', 'r') as f:
+            for line in f.readlines():
+                list_of_sensor_ids.append(line.strip())
+                
+        print(list_of_sensor_ids, sensor_id)
+        if sensor_id not in list_of_sensor_ids:
+            flash('Sensor ID does not exist', category='error')
+        else:
+            user = User.query.filter_by(sensor_id=sensor_id).first()
+            if user:
+                flash('Logged in!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                new_user = User(sensor_id=sensor_id)
+                db.session.add(new_user)
+                db.session.commit()
+                login_user(new_user, remember=True)
+                flash('Logged in!', category='success')
+                return redirect(url_for('views.home'))
+        
+    return render_template("login.html", user=current_user)
