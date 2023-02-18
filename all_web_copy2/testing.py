983eb2507b146cc2cff41a78124f628f16eeb4ca
Jonas Tenora <99197211+wiserat@users.noreply.github.com>
2023-02-18 10:35:34 +0100

    Delete all_web_copy2 directory

--- all_web_copy2/testing.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-r = requests.get('https://data.sensor.community/static/v1/data.json').json()
-
-for item in r:
-    if item['location']['country'] == 'CZ':
-        if item['sensor']['id'] == 43114:   #temp hum press pressatsea 33067
-                                            print(item)
