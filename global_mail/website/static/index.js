075c4b9b47a900af6b311c01f5e4d6bac9715dd8
Jonas Tenora <99197211+wiserat@users.noreply.github.com>
2023-02-18 10:35:47 +0100

    Delete global_mail directory

--- global_mail/website/static/index.js
+++
@@ -1,8 +0,0 @@
-function deleteEmail(emailId) {
-  fetch("/delete-email", {
-    method: "POST",
-    body: JSON.stringify({ emailId: emailId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
-}
