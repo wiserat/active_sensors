616bac78e45e969669a893a4ee2df82fe3f6f84e
Jonas Tenora <99197211+wiserat@users.noreply.github.com>
2023-02-18 10:35:59 +0100

    Delete one_by_one directory

--- one_by_one/config.py
+++
@@ -1,7 +0,0 @@
-conf_url_pm_sensor = "http://api.luftdaten.info/static/v2/data.1h.json"
-conf_url_th_sensor = "http://api.luftdaten.info/static/v2/data.1h.json"
-
-
-smichov_name = "smichov"
-smichov_conf_particle_sensor_id = 73737
-smichov_conf_temperature_sensor_id = 73738
